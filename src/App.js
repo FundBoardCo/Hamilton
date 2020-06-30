70d096f6ea8a2c36da03116627a569b531e79e6f
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-06-30 09:59:48 -0400

    nav and initial pages

--- src/App.js
+++ src/App.js
@@ -1,77 +1,103 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
-  faCalculator,
-  faChartLine,
-  faCogs,
+  faCog,
   faFileCsv,
   faFileExport,
   faFlag,
-  faIndustry,
   faLink,
   faList,
   faMapMarkerAlt,
   faPlus,
   faPlusCircle,
-  faRedoAlt,
-  faSyncAlt,
-  faToggleOn,
-  faToggleOff,
+  faSearch,
 } from '@fortawesome/free-solid-svg-icons';
-import ScrollToTop from './components/ScrollToTop';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from './imgs/FundBoard_Logo.svg';
+import ScrollToTop from './components/ScrollToTop';
 import Intro from './pages/start/Intro';
 import Board from './pages/board/Board';
 import Search from './pages/search/Search';
+import Profile from './pages/profile/Profile';
+import NotFound from './pages/NotFound';
 
 // import common icons so they're accessible later.
 library.add(
   fab,
-  faCalculator,
-  faChartLine,
-  faCogs,
+  faCog,
   faFileCsv,
   faFileExport,
   faFlag,
-  faIndustry,
   faLink,
   faList,
   faMapMarkerAlt,
   faPlus,
   faPlusCircle,
-  faRedoAlt,
-  faSyncAlt,
-  faToggleOn,
-  faToggleOff,
+  faSearch,
 );
 
-const newUser = true; //TODO: base this off of the redux state
+const newUser = false; //TODO: base this off of the redux state
 
 function App() {
   return (
     <Router>
       <ScrollToTop>
-        <Navbar expand="lg" className="fixed-top" id="Nav">
+        <Navbar id="Nav">
           <img id="NavLogo" src={Logo} alt="FundBoard Logo" />
-          <span className="brandName">FundBoard</span>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto" defaultActiveKey={window.location.pathname}>
-              <Nav.Link as={NavLink} href="/search" to="/search" activeClassName="active">Search</Nav.Link>
-              <Nav.Link as={NavLink} href="/board" to="/board">Board</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <span id="NavName">FundBoard</span>
+          <span id="NavVersion">0.1</span>
+          <Nav className="ml-auto" defaultActiveKey={window.location.pathname}>
+            <Nav.Link
+              as={NavLink}
+              href="/board"
+              to="/board"
+              className='board'
+            >
+              <FontAwesomeIcon icon="list" />
+              <span>My FundBoard</span>
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              href="/search"
+              to="/search"
+              className='search'
+              activeClassName="active"
+            >
+              <FontAwesomeIcon icon="search" />
+              <span>Search</span>
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              href="/profile"
+              to="/profile"
+              className='profile'
+            >
+              <FontAwesomeIcon icon="cog" />
+              <span>My Profile</span>
+            </Nav.Link>
+          </Nav>
         </Navbar>
-        <main className="container-xl" id="MainContainer">
-          <Route path="/" exact>
-            {newUser ? <Redirect to="/intro" /> : <Board/>}
-          </Route>
-          <Route path="/board" component={Board} />
-          <Route path="/search" component={Search} />
+        <main className="container-xl" id="Main">
+          <Switch>
+            <Route path="/" exact>
+              {newUser ? <Redirect to="/intro" /> : <Redirect to="/board" />}
+            </Route>
+            <Route path="/intro" component={Intro} />
+            <Route path="/board" component={Board} />
+            <Route path="/search" component={Search} />
+            <Route path="/profile" component={Profile} />
+            <Route component={NotFound} />
+          </Switch>
         </main>
       </ScrollToTop>
     </Router>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function NotFound() {
+  return (
+    <Row id="pageContainer">
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>My FundBoard</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            Placeholder for not found
+          </Col>
+        </Row>
+      </Container>
+    </Row>
+  );
+}
