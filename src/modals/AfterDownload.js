f57222c44e07ddb29ec2a45e74ec71e9d094ecd2
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-08-11 15:33:12 -0400

    hook after download to webflow info source

--- src/modals/AfterDownload.js
+++ src/modals/AfterDownload.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Modal from 'react-bootstrap/Modal';
 import * as types from '../actions/types';
+import DissmissibleStatus from '../components/DissmissibleStatus';
 
 export default function AfterDownload() {
+  const itemId = '5f32b22bf8e8020679adebfc';
+  const item = useSelector(state => state.info[itemId]) || {};
+  const { status, data = {} } = item;
+  console.log(status)
+  console.log(data)
+
   const dispatch = useDispatch();
 
   const closeModal = () => dispatch({
     type: types.MODAL_SET_OPEN,
     modal: null,
   });
+
+  useEffect(() => {
+    dispatch({
+      type: 'INFO_GET_REQUEST',
+      params: { itemId },
+    });
+  }, [dispatch, itemId]);
 
   return (
     <Modal
@@ -23,11 +37,17 @@ export default function AfterDownload() {
     >
       <Modal.Header closeButton>
         <Modal.Title>
-          What do I do with my FundBoard?
+          <h2>{data.name}</h2>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        test
+        <DissmissibleStatus
+          status={status}
+          showSuccess={false}
+          dissmissAction={types.SEARCH_GET_RESULTS_DISMISSED}
+          dismissParams={{ itemId }}
+        />
+        <div dangerouslySetInnerHTML={{ __html: data['tip-body'] }} />
       </Modal.Body>
     </Modal>
   );
