8161578b804898a7403cc1d52a8b29ec74264334
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-08-12 11:00:28 -0400

    update zipcodeapi key

--- src/constants.js
+++ src/constants.js
@@ -9,4 +9,4 @@ export const INTROSEARCH_STAGES = ['keywords', 'raise', 'location'];
 
 export const WEBFLOW_APIKEY = '2e8fe02c04a1efed83d95be4a36ba6bf201fbdef86d7c3b449eba7268c694302';
 
-export const ZIPCODEAPI = 'I0yeusjBefRWL10bRcJsGWWaqidCPmVGEZCmCTJWSpAJiy5EKQxZF2jMooDWs17J';
+export const ZIPCODEAPI = 'SpyunvdJlwQo1PV3zN9EvTeDNkuiWXOJ0QHazIZ2QDrTIHMNTW9AbITZGnUCVUrK';
