c6fd21ebb4c67cfaf5a43c3433b4b02372e58891
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-08-28 15:49:06 -0400

    use hardSet in rehydrate

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 import airtable from './reducers/airtable';
@@ -20,6 +21,7 @@ const persistConfig = {
 const airtableConfig = {
   key: 'airtable',
   storage,
+  stateReconciler: hardSet,
   blacklist: ['feedback_status'],
 };
 
