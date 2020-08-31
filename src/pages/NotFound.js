d16c17f8a89448fd3d0018b35e34ca9b0d8711fc
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-08-31 12:10:39 -0400

    improve 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function NotFound() {
+  const loggedIn = useSelector(state => state.user.token);
+
   return (
     <Row className="pageContainer">
       <Container fluid>
         <Row>
           <Col>
-            <h1>My FundBoard</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            Placeholder for not found
+            <h1 className="text-center">Whoops</h1>
+            <p className="text-center">
+              There doesn&apos;t seem to be a page at this link.
+            </p>
+            <p className="text-center">
+              <a href="/">Go to the index page.</a>
+            </p>
+            {loggedIn && (
+              <p className="text-center">
+                <a href="/board">Go to your FundBoard.</a>
+              </p>
+            )}
           </Col>
         </Row>
       </Container>
