4ba28b60667a3023ae56a63dd2165614d1b1f4c0
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-09-01 12:48:59 -0400

    text and layout tweaks to intro page

--- src/pages/start/Intro.js
+++ src/pages/start/Intro.js
@@ -5,32 +5,24 @@ import Logo from '../../imgs/FundBoard_Logo.svg';
 export default function Intro() {
   return (
     <Row id="PageIntro" className="pageContainer">
-      <div className="introTop">
-        <img id="Logo" className="responsiveImg" src={Logo} alt="FundBoard Logo" />
-        <h1 className="mb-lg-2 h3 h-lg-1">FundBoard</h1>
-        <p className="text-center h3 h-sm-4">Where Founders Manage Their Raise</p>
-        <p className="text-center h5">Find lead investors and track your progress on your FundBoard</p>
+      <div className="introTop mt-5 mt-lg-4 mb-4">
+        <img
+          id="Logo"
+          className="responsiveImg mb-3"
+          src={Logo}
+          alt="FundBoard Logo"
+        />
+        <h1 className="mb-2 mb-lg-1 h2 h-lg-1">FundBoard</h1>
+        <p className="text-center h5 unbold h-lg-3">Where Founders Manage Their Raise</p>
       </div>
       <div className="introBottom">
         <a
           href="/introSearch"
-          className="btn btn-secondary btnNoMax mb-3 mb-lg-4"
+          className="btn btn-secondary btnNoMax mb-3 mb-lg-5"
           data-track="IntroPageSearch"
         >
-          Build your FundBoard
+          Find Your Investors
         </a>
-        <p className="txSm tx-lg-tx text-center">
-          <i>If you&apos;re not raising right now, you can still prep with a Fundboard,</i>
-          <br />
-          <a
-            href="https://fundboard.co"
-            data-track="IntroPageLearn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i>or click here for more resources on building and funding startups.</i>
-          </a>
-        </p>
       </div>
     </Row>
   );
