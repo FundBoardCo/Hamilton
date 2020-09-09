3d25c1e406fff65d8879f9113462e89ee898dd08
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2020-09-09 13:11:06 -0400

    load google analytics in index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 import {
   BrowserRouter as Router,
   Route,
@@ -56,9 +55,6 @@ import Talk from './pages/talk/Talk';
 import NotFound from './pages/NotFound';
 import Modal from './modals/ModalWrapper';
 import * as types from './actions/types';
-
-ReactGA.initialize('UA-163041446-2');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 // import common icons so they're accessible later.
 library.add(
