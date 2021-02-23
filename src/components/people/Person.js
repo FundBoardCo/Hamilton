3d6e1d07d2c0acca2324699bad4fbcf8615fb394
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2021-02-23 16:33:05 -0500

    Show intro number instead of published on board

--- src/components/people/Person.js
+++ src/components/people/Person.js
@@ -87,6 +87,8 @@ export default function Person(props) {
     history.push(`/${path}/${uuid}`);
   };
 
+  const { intros = {} } = investorStatus;
+  const introsLength = Object.keys(intros).length;
   const investorStage = investorStatus.stage || (isOnBoard && 'added');
   let { notes } = investorStatus;
   let next = {};
@@ -175,7 +177,7 @@ export default function Person(props) {
       </button>
       {isBoard && sortedBy !== 'next' && (
         <div className="notesWrapper">
-          <div className='flex-grow-1'>
+          <div className="flex-grow-1">
             <div className="notes text-primary">
               {`Notes(${notes.length})${notes.length > 0 ? `: ${notes[0].text}` : ''}`}
             </div>
@@ -197,15 +199,9 @@ export default function Person(props) {
             ))}
           </div>
           <div className="published">
-            {investorStatus.published ? (
+            {introsLength > 0 && (
               <span>
-                <FontAwesomeIcon icon="eye" className="mr-1" />
-                <span>Public</span>
-              </span>
-            ) : (
-              <span>
-                <FontAwesomeIcon icon="eye-slash" className="mr-1" />
-                <span>Private</span>
+                {`${introsLength} intros offered.`}
               </span>
             )}
           </div>
@@ -281,4 +277,16 @@ Person.propTypes = {
           .oneOfType([PropTypes.string, PropTypes.bool]))),
     intro: PropTypes.objectOf(PropTypes.string),
   }),
+};
+
+Matches.defaultProps = {
+  keywords: [],
+  raise: false,
+  location: false,
+};
+
+Matches.propTypes = {
+  keywords: PropTypes.arrayOf(PropTypes.string),
+  raise: PropTypes.bool,
+  location: PropTypes.bool,
 };
