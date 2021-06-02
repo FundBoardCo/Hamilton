2a1d246f9fd906c7b00b45b8e9dbcea2e4f5eec0
Vernon Mullen <vernonmu@gmail.com>
2021-06-02 01:31:47 -0500

    chore: extract btn property definitions to separate profile util file.

---
+++ src/utils/profileUtils.js
@@ -0,0 +1,24 @@
+const btnProps = {
+  update: {
+    variant: 'secondary',
+    text: 'Update my account',
+    disabled: false,
+  },
+  updateFounderData: {
+    variant: 'secondary',
+    text: 'Update my public profile',
+    disabled: false,
+  },
+  logout: {
+    variant: 'info',
+    text: 'Log out',
+    disabled: false,
+  },
+  delete: {
+    variant: 'warning',
+    text: 'Delete my account',
+    disabled: false,
+  },
+};
+
+export default btnProps;
