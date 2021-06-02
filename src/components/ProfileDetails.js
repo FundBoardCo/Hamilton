284ee15419fc518783e4845ec42eb2a4246429f9
Vernon Mullen <vernonmu@gmail.com>
2021-06-02 01:30:01 -0500

    refactor: consolidate form inputs, keep input handler, move methods from parent to profileDetails component.

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -1,34 +1,187 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Form, Button } from 'react-bootstrap';
 import FormInput from './FormInput';
 import DismissibleStatus from './DismissibleStatus';
+import btnProps from '../utils/profileUtils';
+import LinkInput from './LinkInput';
 import * as types from '../actions/types';
 
+const profileInputs = [
+  {
+    field: 'name',
+    label: 'Your Full Name',
+    placeholder: 'name',
+    section: 'one',
+  },
+  {
+    field: 'title',
+    label: 'Your Title At Your Startup',
+    placeholder: 'title',
+    section: 'one',
+  },
+  {
+    field: 'orgName',
+    label: 'The Name of Your Startup',
+    placeholder: 'startup name',
+    section: 'one',
+  },
+  {
+    field: 'orgURL',
+    label: 'Your Startup’s Website',
+    type: 'url',
+    placeholder: 'http://something.com',
+    feedback: true,
+    section: 'one',
+  },
+  {
+    field: 'orgLogoURL',
+    label: 'A Link to Your Startup’s Logo',
+    type: 'url',
+    placeholder: 'http://something.com/something.jpg',
+    formText: 'You should link to an image that is at least 100 x 100 px, but not larger than '
+      + '400 x 400.',
+    feedback: true,
+    section: 'one',
+  },
+  {
+    field: 'linkedin',
+    label: 'Your LinkedIn Page',
+    type: 'url',
+    placeholder: 'LinkedIn url',
+    feedback: true,
+    section: 'two',
+  },
+  {
+    field: 'twitter',
+    label: 'Your Twitter Page',
+    type: 'url',
+    placeholder: 'Twitter url',
+    feedback: true,
+    section: 'two',
+  },
+  {
+    field: 'permalink',
+    label: 'Your CrunchBase Page',
+    type: 'url',
+    placeholder: 'CrunchBase url',
+    feedback: true,
+    section: 'two',
+  },
+  {
+    field: 'team_size',
+    label: 'How Many People Are On Your Team?',
+    type: 'number',
+    min: 1,
+    placeholder: 'number equal to or higher than 1',
+    feedback: true,
+    section: 'three',
+  },
+  {
+    field: 'location_city',
+    label: 'Your City (HQ, or personal if fully remote)',
+    placeholder: 'City',
+    section: 'three',
+  },
+  {
+    field: 'location_state',
+    label: 'Your State',
+    placeholder: 'XX',
+    formText: 'Use a 2 letter state abbreviation.',
+    pattern: '[A-Z]{2}',
+    section: 'three',
+  },
+  {
+    field: 'remote',
+    label: 'We’re fully remote',
+    type: 'checkbox',
+    section: 'three',
+  },
+];
+
 const ProfileDetails = props => {
-  window.console.log('props are ', props);
-  const {
-    publicValidated,
-    handlePublicSubmit,
-    publicInputs1,
-    onInputChange,
-    showPublicInputs,
-    inputState,
-    publicInputs2,
-    publicInputs3,
-    currentUpdate,
-    LinkInput,
-    setLinkText,
-    setLinkURL,
-    removeLink,
-    addLink,
-    updateStatus,
-    setShowPublicInputs,
-    btnProps,
-    updateProfileStatus,
-    profile,
-  } = props;
+  const { profile } = props;
+  const [inputState, setInputState] = useState({});
+  const [currentUpdate, setCurrentUpdate] = useState();
+  const [publicValidated, setPublicValidated] = useState(false);
+  const [showPublicInputs, setShowPublicInputs] = useState(false);
+  const updateProfileStatus = useSelector(state => state.user.updateProfile_status);
+  const updateStatus = useSelector(state => state.user.update_status);
+
+  const publicInputs1 = profileInputs.filter(el => el.section === 'one');
+  const publicInputs2 = profileInputs.filter(el => el.section === 'two');
+  const publicInputs3 = profileInputs.filter(el => el.section === 'three');
+
+  const dispatch = useDispatch();
+
+  const updateFounderData = params => dispatch({
+    type: types.USER_POST_PROFILE_REQUESTED,
+    params,
+  });
+
+  const onInputChange = e => {
+    // eslint-disable-next-line no-shadow
+    const { name, type, checked } = e.target;
+    let { value } = e.target;
+    if (type === 'checkbox') value = !!checked;
+    if (type === 'number') value = Number(value);
+
+    setInputState(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handlePublicSubmit = event => {
+    setCurrentUpdate('public');
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    setPublicValidated(true);
+    if (form.checkValidity() !== false) {
+      const params = {
+        primary_job_title: inputState.title,
+        primary_organization_name: inputState.orgName,
+        primary_organization_homepage: inputState.orgURL,
+        primary_organization_logo: inputState.orgLogoURL,
+        description: inputState.desc,
+        remote: !!inputState.remote,
+        ...inputState,
+      };
+      updateFounderData(params);
+    }
+  };
+
+  const setLinkText = (text, index) => {
+    const newLinks = [...inputState.links];
+    newLinks[index].text = text;
+    setInputState(prevState => ({ ...prevState, links: newLinks }));
+  };
+
+  const setLinkURL = (url, index) => {
+    const newLinks = [...inputState.links];
+    newLinks[index].url = url;
+    setInputState(prevState => ({ ...prevState, links: newLinks }));
+  };
+
+  const removeLink = i => {
+    const newLinks = [...inputState.links];
+    newLinks.splice(i, 1);
+    setInputState(prevState => ({ ...prevState, links: newLinks }));
+  };
+
+  const addLink = () => {
+    const newLinks = [
+      ...(inputState.links ? inputState.links : []),
+      {
+        text: '',
+        url: '',
+        key: Math.floor(Math.random() * Math.floor(1000000)),
+      },
+    ];
+    setInputState(prevState => ({ ...prevState, links: newLinks }));
+  };
+
+  const userLinks = inputState.links && inputState.length ? inputState.links : profile.links;
 
   return (
     <Form
@@ -37,20 +190,19 @@ const ProfileDetails = props => {
       validated={publicValidated}
       onSubmit={handlePublicSubmit}
     >
-      {Object.keys(publicInputs1).map(k => {
-        const inputData = publicInputs1;
+      {publicInputs1.map(k => {
+        const inputData = k;
 
-        if (Object.keys(profile).includes(k)) {
-          window.console.log('bingo ', k);
-          inputData[k].value = profile[k];
+        if (Object.keys(profile).includes(k.field) && !!(k.field)) {
+          inputData.value = inputState[k.field] || profile[k.field];
         }
 
         return (
           <FormInput
             onChange={onInputChange}
-            key={k}
-            iKey={k}
-            {...inputData[k]}
+            key={k.field}
+            iKey={k.field}
+            {...inputData}
           />
         );
       })}
@@ -62,31 +214,41 @@ const ProfileDetails = props => {
               as="textarea"
               placeholder="More information about you that would be relevant to someone making an intro."
               name="desc"
-              value={inputState.desc}
+              value={inputState.desc || profile.desc}
               onChange={e => onInputChange(e)}
               data-track="ProfileDescription"
             />
           </Form.Group>
-          {Object.keys(publicInputs2).map(k => (
-            <FormInput
-              onChange={onInputChange}
-              key={k}
-              iKey={k}
-              {...publicInputs2[k]}
-            />
-          ))}
+          {publicInputs2.map(k => {
+            const inputData = k;
+
+            if (Object.keys(profile).includes(k.field) && !!(k.field)) {
+              inputData.value = inputState[k.field] || profile[k.field];
+            }
+
+            return (
+              <FormInput
+                onChange={onInputChange}
+                key={k.field}
+                iKey={k.field}
+                {...k}
+              />
+            );
+          })}
           <h5>Additional Links</h5>
-          {inputState.links.map((l, i) => (
-            <LinkInput
-              text={l.text}
-              url={l.url}
-              key={l.key}
-              linkIndex={i}
-              onLinkTextChange={setLinkText}
-              onLinkURLChange={setLinkURL}
-              onLinkRemove={removeLink}
-            />
-          ))}
+          {
+            userLinks.map((l, i) => (
+              <LinkInput
+                text={l.text}
+                url={l.url}
+                key={l.key}
+                linkIndex={i}
+                onLinkTextChange={setLinkText}
+                onLinkURLChange={setLinkURL}
+                onLinkRemove={removeLink}
+              />
+            ))
+          }
           <Button
             variant="link"
             className="text-secondary mb-4"
@@ -96,14 +258,22 @@ const ProfileDetails = props => {
           >
             Add another link
           </Button>
-          {Object.keys(publicInputs3).map(k => (
-            <FormInput
-              onChange={onInputChange}
-              key={k}
-              iKey={k}
-              {...publicInputs3[k]}
-            />
-          ))}
+          {publicInputs3.map(k => {
+            const inputData = k;
+
+            if (Object.keys(profile).includes(k.field) && !!(k.field)) {
+              inputData.value = inputState[k.field] || profile[k.field];
+            }
+
+            return (
+              <FormInput
+                onChange={onInputChange}
+                key={k.field}
+                iKey={k.field}
+                {...k}
+              />
+            );
+          })}
         </div>
       )}
       {currentUpdate === 'public' && (
@@ -147,83 +317,10 @@ const ProfileDetails = props => {
 };
 
 ProfileDetails.defaultProps = {
-  publicValidated: true,
-  inputState: {},
-  publicInputs1: '',
-  onInputChange: '',
-  showPublicInputs: true,
-  publicInputs2: '',
-  publicInputs3: '',
-  currentUpdate: '',
-  LinkInput: () => {},
-  setLinkText: () => {},
-  setLinkURL: () => {},
-  removeLink: () => {},
-  addLink: () => {},
-  updateStatus: () => {},
-  setShowPublicInputs: () => {},
-  btnProps: {},
-  handlePublicSubmit: () => {},
-  updateProfileStatus: () => {},
-  profile: () => {},
+  profile: '',
 };
 
 ProfileDetails.propTypes = {
-  publicValidated: PropTypes.bool,
-  inputState: PropTypes.shape({
-    desc: PropTypes.string,
-    linkedin: PropTypes.string,
-    links: PropTypes.arrayOf(PropTypes.string),
-    location_city: PropTypes.string,
-    location_state: PropTypes.string,
-    name: PropTypes.string,
-    orgLogoURL: PropTypes.string,
-    orgName: PropTypes.string,
-    orgURL: PropTypes.string,
-    password: PropTypes.string,
-    permalink: PropTypes.string,
-    remote: PropTypes.bool,
-    team_size: PropTypes.number,
-    title: PropTypes.string,
-    twitter: PropTypes.string,
-  }),
-  publicInputs1: PropTypes.string,
-  onInputChange: PropTypes.func,
-  showPublicInputs: PropTypes.string,
-  publicInputs2: PropTypes.bool,
-  publicInputs3: PropTypes.string,
-  currentUpdate: PropTypes.string,
-  LinkInput: PropTypes.func,
-  setLinkText: PropTypes.func,
-  setLinkURL: PropTypes.func,
-  removeLink: PropTypes.func,
-  addLink: PropTypes.func,
-  updateStatus: PropTypes.func,
-  setShowPublicInputs: PropTypes.func,
-  handlePublicSubmit: PropTypes.func,
-  btnProps: PropTypes.shape({
-    update: PropTypes.shape({
-      variant: PropTypes.string,
-      text: PropTypes.string,
-      disabled: PropTypes.bool,
-    }),
-    updateFounderData: PropTypes.shape({
-      variant: PropTypes.string,
-      text: PropTypes.string,
-      disabled: PropTypes.bool,
-    }),
-    logout: PropTypes.shape({
-      variant: PropTypes.string,
-      text: PropTypes.string,
-      disabled: PropTypes.bool,
-    }),
-    delete: PropTypes.shape({
-      variant: PropTypes.string,
-      text: PropTypes.string,
-      disabled: PropTypes.bool,
-    }),
-  }),
-  updateProfileStatus: PropTypes.func,
   profile: PropTypes.shape({
     createdAt: PropTypes.string,
     desc: PropTypes.string,
